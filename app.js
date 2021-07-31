d7a2a85dd3014bc39cf5a1f8c552ccc7038625f1
Mustafa Iram <mustafaudoy@gmail.com>
2021-07-31 16:30:34 +0600

    egula dekh

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let control = document.getElementById("b1");
+let div = document.getElementById("history");
+
+control.addEventListener('click', () => {
+   if(div.style.display=== 'none')
+      {
+        div.style.display = 'block';
+      }
+      //else
+      //{
+        //div.style.display ===  'none';
+      //}
+    
+
+});
