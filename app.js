8c1adc5a5a594f4abbee4f2586870ba532d5e83d
Mustafa Iram <mustafaudoy@gmail.com>
2021-07-31 17:44:41 +0600

    notun

--- app.js
+++ app.js
@@ -1,16 +1,74 @@
 
-let control = document.getElementById("b1");
-let div = document.getElementById("history");
-
-control.addEventListener('click', () => {
-   if(div.style.display=== 'none')
+document.getElementById("b1").addEventListener("click", function()
+{
+    document.getElementById("b2").style.visibility = 'hidden';
+    document.getElementById("b3").style.visibility = 'hidden';
+    document.getElementById("b4").style.visibility = 'hidden';
+    if(document.getElementById("history").style.display=== 'none')
       {
-        div.style.display = 'block';
+        document.getElementById("history").style.display = 'block';
       }
-      //else
-      //{
-        //div.style.display ===  'none';
-      //}
+      else
+      {
+        document.getElementById("history").style.display ===  'none';
+      }
+}
+
+
+);
+document.getElementById("b2").addEventListener("click", function()
+{
+    document.getElementById("b1").style.visibility = 'hidden';
+    document.getElementById("b3").style.visibility = 'hidden';
+    document.getElementById("b4").style.visibility = 'hidden';
+    if(document.getElementById("tours").style.display=== 'none')
+      {
+        document.getElementById("tours").style.display = 'block';
+      }
+      else
+      {
+        document.getElementById("tours").style.display ===  'none';
+      }
+}
+
+
+);
+document.getElementById("b3").addEventListener("click", function()
+{
+    document.getElementById("b1").style.visibility = 'hidden';
+    document.getElementById("b2").style.visibility = 'hidden';
+    document.getElementById("b4").style.visibility = 'hidden';
+    if(document.getElementById("albums").style.display=== 'none')
+      {
+        document.getElementById("albums").style.display = 'block';
+      }
+      else
+      {
+        document.getElementById("albums").style.display ===  'none';
+      }
+}
+
+
+);
+document.getElementById("b4").addEventListener("click", function()
+{
+    document.getElementById("b1").style.visibility = 'hidden';
+    document.getElementById("b2").style.visibility = 'hidden';
+    document.getElementById("b3").style.visibility = 'hidden';
+    if(document.getElementById("contact").style.display=== 'none')
+      {
+        document.getElementById("contact").style.display = 'block';
+      }
+      else
+      {
+        document.getElementById("contact").style.display ===  'none';
+      }
+}
+
+
+);
+
+
+   
     
 
-});
